851d91e78da1b7241dacec9076920ed384c4ed67
naelstrof <naelstrof@gmail.com>
2021-08-24 16:23:59 -0600

    fixed build error

--- Scripts/LayerAttribute.cs
+++ Scripts/LayerAttribute.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace PenetrationTech {
     public class LayerAttribute : PropertyAttribute { }
+#if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(LayerAttribute))]
     public class LayerAttributeDrawer : PropertyDrawer {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             property.intValue = EditorGUI.LayerField(position, label, property.intValue);
         }
     }
-}
 #endif
+}
