d1e8148a4c0378099d6033949d48dedfa15d7ae9
Naelstrof <naelstrof@gmail.com>
2022-05-17 17:48:57 -0600

    Parallel transport frame implementation.

--- Scripts/CatmullDebugger.cs
+++ Scripts/CatmullDebugger.cs
@@ -35,24 +35,8 @@ public class CatmullDebugger : MonoBehaviour {
         Matrix4x4 savedMatrix = Gizmos.matrix;
         int frames = 128;
         for (int i=0;i<frames;i++) {
-            // https://en.wikipedia.org/wiki/Frenet%E2%80%93Serret_formulas
-            // https://janakiev.com/blog/framing-parametric-curves/
             float t = (float)i/(float)frames;
-            Vector3 point = path.GetPositionFromT(t);
-            Vector3 tangent = path.GetTangentFromT(t).normalized;
-            Vector3 binormal = Vector3.Cross(path.GetTangentFromT(t),path.GetAccelerationFromT(t)).normalized;
-            Vector3 normal = Vector3.Cross(binormal, tangent);
-
-            // Change of basis https://math.stackexchange.com/questions/3540973/change-of-coordinates-and-change-of-basis-matrices
-            Matrix4x4 BezierBasis = new Matrix4x4();
-            BezierBasis.SetRow(0,binormal); // Our X axis
-            BezierBasis.SetRow(1,normal); // Y Axis
-            BezierBasis.SetRow(2,tangent); // Z Axis
-            BezierBasis[3,3] = 1f;
-            // Change of basis formula is B = P⁻¹ * A * P, where P is the basis transform.
-            Matrix4x4 ToBezierSpace = Matrix4x4.Translate(point)*BezierBasis.inverse;
-
-            Gizmos.matrix = ToBezierSpace;
+            Gizmos.matrix = path.GetReferenceFrameFromTime(t);
             //Gizmos.DrawMesh(debugMesh);
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(Vector3.zero, Vector3.forward*0.2f);
@@ -60,7 +44,7 @@ public class CatmullDebugger : MonoBehaviour {
             Gizmos.DrawLine(Vector3.zero, Vector3.right*0.2f);
             Gizmos.color = Color.green;
             Gizmos.DrawLine(Vector3.zero, Vector3.up*0.2f);
-            //Gizmos.DrawLine(point, point+tangent*0.1f);*/
+            //Gizmos.DrawLine(point, point+tangent*0.1f);
         }
         Gizmos.matrix = savedMatrix;
     }
--- Scripts/CatmullPath.cs
+++ Scripts/CatmullPath.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace PenetrationTech {
-    
-    public static class ExtensionFloat {
-        public static float Remap (this float value, float from1, float to1, float from2, float to2) {
+    public class CatmullPath {
+        public static float Remap (float value, float from1, float to1, float from2, float to2) {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
-    }
-
-    public class CatmullPath {
-
         private static Vector3 GetPosition(Vector3 start, Vector3 tanPoint1, Vector3 tanPoint2, Vector3 end, float t) {
             // Using the expanded form of a Hermite basis functions
             // https://en.wikipedia.org/wiki/Cubic_Hermite_spline
@@ -40,26 +35,26 @@ namespace PenetrationTech {
                 + (6f * t - 2f) * tanPoint2;
             return curvature;
         }
-        
         private List<Vector3> weights;
         private List<Vector3> points;
-        private List<float> LUT;
+        private List<float> distanceLUT;
+        private List<Vector3> binormalLUT;
         public float arcLength {get; private set;}
-
         public List<Vector3> GetWeights() => weights;
-        public List<float> GetDistanceLUT() => LUT;
+        public List<float> GetDistanceLUT() => distanceLUT;
 
         public CatmullPath(Vector3[] newPoints) {
             points = new List<Vector3>();
             weights = new List<Vector3>();
-            LUT = new List<float>();
+            distanceLUT = new List<float>();
+            binormalLUT = new List<Vector3>();
             SetWeightsFromPoints(newPoints);
         }
 
         private Vector3 SampleCurveSegmentPosition(int curveSegmentIndex, float t) {
             return GetPosition(weights[curveSegmentIndex*4], weights[curveSegmentIndex*4+1], weights[curveSegmentIndex*4+2], weights[curveSegmentIndex*4+3], t);
         }
-        private Vector3 SampleCurveSegmentTangent(int curveSegmentIndex, float t) {
+        private Vector3 SampleCurveSegmentVelocity(int curveSegmentIndex, float t) {
             return GetVelocity(weights[curveSegmentIndex*4], weights[curveSegmentIndex*4+1], weights[curveSegmentIndex*4+2], weights[curveSegmentIndex*4+3], t);
         }
         private Vector3 SampleCurveSegmentAcceleration(int curveSegmentIndex, float t) {
@@ -74,26 +69,57 @@ namespace PenetrationTech {
         
         private float DistToTime(float distance) {
             if (distance > 0f && distance < arcLength) {
-                for(int i=0;i<LUT.Count-1;i++) {
-                    if (distance>LUT[i] && distance<LUT[i+1]) {
-                        return distance.Remap(LUT[i],LUT[i+1],(float)i/(LUT.Count-1f),(float)(i+1)/(LUT.Count-1f));
+                for(int i=0;i<distanceLUT.Count-1;i++) {
+                    if (distance>distanceLUT[i] && distance<distanceLUT[i+1]) {
+                        return Remap(distance,distanceLUT[i],distanceLUT[i+1],(float)i/(distanceLUT.Count-1f),(float)(i+1)/(distanceLUT.Count-1f));
                     }
                 }
             }
             return distance/arcLength;
         }
-        private void GenerateLUT(int resolution) {
+        private void GenerateDistanceLUT(int resolution) {
             float dist = 0f;
             Vector3 lastPosition = SampleCurveSegmentPosition(0, 0f);
-            LUT.Clear();
+            distanceLUT.Clear();
             for(int i=0;i<resolution;i++) {
                 float t = (((float)i)/(float)resolution);
                 Vector3 position = GetPositionFromT(t);
                 dist += Vector3.Distance(lastPosition, position);
                 lastPosition = position;
-                LUT.Add(dist);
+                distanceLUT.Add(dist);
             }
             arcLength = dist;
+        }
+        private void GenerateNormalLUT(int resolution) {
+            // https://en.wikipedia.org/wiki/Frenet%E2%80%93Serret_formulas
+            // https://janakiev.com/blog/framing-parametric-curves/
+            binormalLUT.Clear();
+            Vector3 lastTangent = GetVelocityFromT(0).normalized;
+            // Initial reference frame, uses Vector3.up
+            Vector3 lastBinormal = Vector3.Cross(GetVelocityFromT(0),Vector3.up).normalized;
+            for(int i=0;i<resolution;i++) {
+                float t = (((float)i)/(float)resolution);
+                Vector3 point = GetPositionFromT(t);
+                Vector3 tangent = GetVelocityFromT(t).normalized;
+                //Vector3 binormal = Vector3.Cross(GetTangentFromT(t),Vector3.up).normalized;
+                Vector3 binormal = Vector3.Cross(lastTangent, tangent);
+                if (binormal.magnitude == 0f) {
+                    binormal = lastBinormal;
+                } else {
+                    float theta = Vector3.Angle(lastTangent, tangent); // Mathf.Acos(Vector3.Dot(lastTangent,tangent))
+                    binormal = Quaternion.AngleAxis(theta,binormal.normalized)*lastBinormal;
+                }
+                lastTangent = tangent;
+                lastBinormal = binormal;
+                binormalLUT.Add(binormal);
+            }
+
+            // Undo any twist.
+            float overallAngle = Vector3.Angle(binormalLUT[0], binormalLUT[resolution-1]);
+            for(int i=0;i<resolution;i++) {
+                float t = (float)i/(float)resolution;
+                binormalLUT[i] = Quaternion.AngleAxis(-overallAngle*t, GetVelocityFromT(t).normalized)*binormalLUT[i];
+            }
         }
         public void SetWeightsFromPoints(Vector3[] newPoints) {
             points.Clear();
@@ -120,7 +146,8 @@ namespace PenetrationTech {
                 weights.Add(m1);
                 weights.Add(p1);
             }
-            GenerateLUT(32);
+            GenerateDistanceLUT(32);
+            GenerateNormalLUT(32);
         }
         public Vector3 GetPositionFromDistance(float distance) {
             float t = DistToTime(distance);
@@ -131,25 +158,46 @@ namespace PenetrationTech {
             float subT = GetCurveSegmentTimeFromCurveTime(out curveSegmentIndex, t);
             return SampleCurveSegmentPosition(curveSegmentIndex, subT);
         }
-        public Vector3 GetTangentFromDistance(float distance) {
+        public Vector3 GetVelocityFromDistance(float distance) {
             float t = DistToTime(distance);
-            return GetTangentFromT(t);
+            return GetVelocityFromT(t);
         }
         public Vector3 GetAccelerationFromDistance(float distance) {
             float t = DistToTime(distance);
             return GetAccelerationFromT(t);
         }
-        public Vector3 GetTangentFromT(float t) {
+        public Vector3 GetVelocityFromT(float t) {
             int curveSegmentIndex;
             float subT = GetCurveSegmentTimeFromCurveTime(out curveSegmentIndex, t);
-            return SampleCurveSegmentTangent(curveSegmentIndex, subT);
+            return SampleCurveSegmentVelocity(curveSegmentIndex, subT);
         }
         public Vector3 GetAccelerationFromT(float t) {
             int curveSegmentIndex;
             float subT = GetCurveSegmentTimeFromCurveTime(out curveSegmentIndex, t);
             return SampleCurveSegmentAcceleration(curveSegmentIndex, subT);
         }
+        public Vector3 GetBinormalFromT(float t) {
+            int index = Mathf.FloorToInt(t*(binormalLUT.Count-1));
+            float offseted = t-((float)index/(float)(binormalLUT.Count-1));
+            float lerpT = offseted * (float)(binormalLUT.Count-1);
+            return Vector3.Lerp(binormalLUT[index], binormalLUT[index+1], lerpT);
+        }
+        public Matrix4x4 GetReferenceFrameFromT(float t) {
+            Vector3 point = GetPositionFromT(t);
+            Vector3 tangent = GetVelocityFromT(t).normalized;
+            Vector3 binormal = GetBinormalFromT(t).normalized;
+            Vector3 normal = Vector3.Cross(tangent, binormal);
 
+            // Change of basis https://math.stackexchange.com/questions/3540973/change-of-coordinates-and-change-of-basis-matrices
+            // It also shows up here: https://docs.unity3d.com/ScriptReference/Vector3.OrthoNormalize.html
+            Matrix4x4 BezierBasis = new Matrix4x4();
+            BezierBasis.SetRow(0,binormal); // Our X axis
+            BezierBasis.SetRow(1,normal); // Y Axis
+            BezierBasis.SetRow(2,tangent); // Z Axis
+            BezierBasis[3,3] = 1f;
+            // Change of basis formula is B = P⁻¹ * A * P, where P is the basis transform.
+            return Matrix4x4.Translate(point)*BezierBasis.inverse;
+        }
     }
 
 }
