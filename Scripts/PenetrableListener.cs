2b0aa9b850639a688f0d7498a133679e1464c702
Naelstrof <naelstrof@gmail.com>
2022-05-21 17:57:41 -0600

    Another listener implementation, some listener implementation changes.

--- Scripts/PenetrableListener.cs
+++ Scripts/PenetrableListener.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace PenetrationTech {
-    public class PenetrableListenerAttribute : System.Attribute { }
+    public class PenetrableListenerAttribute : System.Attribute {
+        public System.Type type;
+        public string name;
+        public PenetrableListenerAttribute(System.Type type, string name) {
+            this.type = type;
+            this.name = name;
+        }
+    }
 
     [System.Serializable]
     public class PenetrableListener {
         [HideInInspector]
-        public float t;
-        public float dist;
-        [HideInInspector]
-        public float penetratedGirth;
-        [HideInInspector]
-        public float penetratedDepth;
+        protected float t;
+        [SerializeField]
+        protected float dist;
         public virtual void OnEnable() { }
         public virtual void OnDisable() { }
-        public virtual void OnPenetrationGirthChange(float newGirth) {
-            penetratedGirth = newGirth;
+        public virtual void Update() { }
+        public virtual float GetDist() {
+            return dist;
         }
-        public virtual void OnPenetrationDepthChange(float newDepth) {
-            penetratedDepth = newDepth;
+        public virtual void OnPenetrationGirthChange(Penetrator penis, float newGirth) {
+        }
+        public virtual void OnPenetrationDepthChange(Penetrator penis, float newDepth) {
         }
         public virtual void OnDrawGizmosSelected(Penetrable p) { }
         public virtual void OnValidate(Penetrable p) {
