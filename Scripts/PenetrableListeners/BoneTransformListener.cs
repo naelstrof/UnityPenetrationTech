107989067361c5fc33f5bab29d6155c160bd4376
Raliv <80774241+Raliv@users.noreply.github.com>
2022-05-21 23:45:22 -0500

    refactor

--- Scripts/PenetrableListeners/BoneTransformListener.cs
+++ Scripts/PenetrableListeners/BoneTransformListener.cs
@@ -38,5 +38,9 @@ namespace PenetrationTech {
             UnityEditor.Handles.DrawWireCube(targetBone.parent.TransformPoint(originalLocalPosition + localOffset), Vector3.one*0.005f);
             #endif
         }
+        public override void NotifyPenetration(Penetrator penetrator, float worldSpaceDistanceToPenisRoot) {
+            NotifyPenetrationGDO(penetrator, worldSpaceDistanceToPenisRoot, false, false, true);
+        }
+
     }
 }
